28824521748903b3b976bf8a722d8f335d91c2b9
罗海方 <15828011064@163.com>
2020-01-17 11:22:49 +0800

    收益排行界面头像的处理

--- 600生活/AppScenes/IncomeRank/View/IncomeRankTableViewCell.m
+++ 600生活/AppScenes/IncomeRank/View/IncomeRankTableViewCell.m
@@ -12,6 +12,7 @@
 @property (weak, nonatomic) IBOutlet UIImageView *icon1; //排行图片
 @property (weak, nonatomic) IBOutlet UILabel *rankLab;  //排名
 @property (weak, nonatomic) IBOutlet UIImageView *userIcon; //用户头像
+@property (weak, nonatomic) IBOutlet UIImageView *userIcon_bg;//用户头像背景圈
 @property (weak, nonatomic) IBOutlet UILabel *nameLab;  //用户名字
 @property (weak, nonatomic) IBOutlet UILabel *incomeLab;//收益
 
@@ -47,33 +48,40 @@
     
     //
     self.nameLab.text = incomeRankModel.username;
+    self.userIcon.layer.cornerRadius = 17.5;
+    self.userIcon.layer.masksToBounds = YES;
     [self.userIcon sd_setImageWithURL:[NSURL URLWithString:incomeRankModel.avatar] placeholderImage:kPlaceHolderUser];
     self.incomeLab.text = [NSString stringWithFormat:@"%@元",incomeRankModel.total_earnings];
 }
 
 -(void)setupIcon1WithIndexPath:(NSIndexPath*)indexPath
 {
+    self.userIcon_bg.layer.cornerRadius = 20.0;
+    self.userIcon_bg.layer.masksToBounds = YES;
+    
     if(indexPath.row == 0){
         self.icon1.image = [UIImage imageNamed:@"排行冠军"];
         self.icon1.hidden = NO;
         [self beginWobbleWithView:self.icon1];
         self.icon1WidthCons.constant = 38;
         self.icon1HeightCons.constant = 38;
+        self.userIcon_bg.backgroundColor = [UIColor colorWithRed:247/255.0 green:195/255.0 blue:62/255.0 alpha:1.0];
     }else if(indexPath.row == 1){
         self.icon1.image = [UIImage imageNamed:@"排行亚军"];
         self.icon1.hidden = NO;
         self.icon1WidthCons.constant = 30;
         self.icon1HeightCons.constant = 30;
-//        [self beginWobbleWithView:self.icon1];
+     self.userIcon_bg.backgroundColor =[UIColor colorWithRed:143/255.0 green:173/255.0 blue:219/255.0 alpha:1.0];
     }else if(indexPath.row == 2){
         self.icon1.image = [UIImage imageNamed:@"排行季军"];
         self.icon1.hidden = NO;
         self.icon1WidthCons.constant = 26;
         self.icon1HeightCons.constant = 26;
-//        [self beginWobbleWithView:self.icon1];
+        self.userIcon_bg.backgroundColor =[UIColor colorWithRed:255/255.0 green:156/255.0 blue:90/255.0 alpha:1.0];
     }else{
         self.icon1.hidden = YES;
         [self endWobbleWithView:self.icon1];
+        self.userIcon_bg.backgroundColor =[UIColor colorWithRed:238/255.0 green:238/255.0 blue:238/255.0 alpha:1.0];
     }
 }
 
